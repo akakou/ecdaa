d5e2881250242b15286d99f8bc63e3f4f63bc781
akakou <akakou571@gmail.com>
2022-05-24 13:21:13 +0900

    [add] types about Issuer's param (i.e. ISK/IPK)

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+
+	"golang.org/x/crypto/bn256"
+)
 
 func main() {
-  fmt.Printf("Hello World\n")
+	fmt.Printf("Hello World\n")
 }
+
+
+
+type ISK struct {
+	x *big.Int
+	y *big.Int
+}
+
+type IPK struct {
+	X   bn256.G2
+	Y   bn256.G2
+	c   big.Int
+	s_x big.Int
+	s_y big.Int
+}
+
+type Issuer struct {
+	ipk *IPK
+	isk *ISK
+}
+
