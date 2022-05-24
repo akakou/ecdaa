fb27ad90ae460d2bb953a8ea644e2e3ccfb60e63
akakou <akakou571@gmail.com>
2022-05-24 13:25:40 +0900

    [add] generate ISK with random

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"crypto/rand"
-	"fmt"
 	"math/big"
 
 	"golang.org/x/crypto/bn256"
 )
 
 func main() {
-	fmt.Printf("Hello World\n")
+	_, err := RandomISK()
+
+	if err != nil {
+		println("err!")
+	}
+
+	println("ok!")
+
 }
 
+func Order() *big.Int {
+	max := new(big.Int)
+	max.Exp(big.NewInt(2), big.NewInt(256), nil).Sub(max, big.NewInt(1))
 
+	return max
+}
+
+var order = Order()
 
 type ISK struct {
 	x *big.Int
@@ -32,3 +45,23 @@ type Issuer struct {
 	isk *ISK
 }
 
+func RandomISK() (*ISK, error) {
+	isk := new(ISK)
+
+	x, err := rand.Int(rand.Reader, order)
+
+	if err != nil {
+		return nil, err
+	}
+
+	y, err := rand.Int(rand.Reader, order)
+
+	if err != nil {
+		return nil, err
+	}
+
+	isk.x = x
+	isk.y = y
+
+	return isk, nil
+}
