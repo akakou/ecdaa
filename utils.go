60dcda5ebc83105abc8335143b717aaa95ff114f
akakou <akakou571@gmail.com>
2022-08-02 16:17:51 +0900

    [add] hash function direct to ecp

--- utils.go
+++ utils.go
@@ -54,3 +54,76 @@ func HashFromBIGToECP(n *FP256BN.BIG) *FP256BN.ECP {
 	g := g1()
 	return g.Mul(n)
 }
+
+func HashToECP(message []byte) {
+	var i uint32
+	isOnCurve := false
+
+	for ; i < 232 && !isOnCurve; i++ {
+		// This process corresponds to BigNumberToB.
+		// Compute BigNumberToB(i,4) on FIDO's spec indicated
+		// that it should continue to find the number which
+		// can be use for calculate the correct value on next step.
+		numBuf := i32tob(i)
+
+		hasher := sha256.New()
+		hasher.Write(numBuf[:])
+		hasher.Write(message[:])
+		hash := hasher.Sum(nil)
+
+		x := FP256BN.FromBytes(hash)
+
+		z := x.Powmod(FP256BN.NewBIGint(3), p())
+		z = x.Plus(b())
+		z.Mod(p())
+
+		y := FP256BN.Modsqr(z, p())
+		ecp := FP256BN.NewECPbigs(x, y)
+
+		isOnCurve = isOnECPCurve(ecp)
+
+		fmt.Printf("y(%v): %v\n", isOnCurve, y)
+	}
+}
+
+func i32tob(val uint32) []byte {
+	r := make([]byte, 4)
+	for i := uint32(0); i < 4; i++ {
+		r[i] = byte((val >> (8 * i)) & 0xff)
+	}
+	return r
+}
+
+func isOnECPCurve(point *FP256BN.ECP) bool {
+	point.Affine()
+
+	zero := FP256BN.NewBIGint(0)
+	copied := FP256BN.NewECPbigs(zero, point.GetY())
+
+	cofactor := FP256BN.NewBIGint(FP256BN.CURVE_Cof_I)
+	unity := FP256BN.NewBIGint(1)
+
+	if FP256BN.Comp(cofactor, unity) == 0 {
+		return true
+	}
+
+	suspectedInf := copied.Mul(cofactor)
+
+	if suspectedInf.Is_infinity() {
+		return false
+	}
+
+	return true
+}
+
+// func reverse(src []FP256BN.Chunk) []FP256BN.Chunk {
+// 	dest := make([]FP256BN.Chunk, len(src))
+
+// 	copy(dest, src)
+
+// 	for i, j := 0, len(dest)-1; i < j; i, j = i+1, j-1 {
+// 		dest[i], dest[j] = dest[j], dest[i]
+// 	}
+
+// 	return dest
+// }
