1c67a8d645e02f1076cb5ab189b32d730b4eb757
akakou <akakou571@gmail.com>
2022-09-07 17:30:57 +0900

    [add] test for comparison

---
+++ compare_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"miracl/core/FP256BN"
+	"testing"
+)
+
+func TestCompECP(t *testing.T) {
+	rng := InitRandom()
+
+	big := FP256BN.Random(rng)
+
+	x := g1()
+	x = x.Mul(big)
+
+	y := g1()
+
+	if compECP(*x, *y) {
+		t.Errorf("unexpected match (`%v, %v`)", x, y)
+	}
+
+	y = y.Mul(big)
+
+	if !compECP(*x, *y) {
+		t.Errorf("not match (`%v, %v`)", x, y)
+	}
+}
