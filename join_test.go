fa52fe1273901fdb5d447b5bcf838fd07d04a97a
akakou <akakou571@gmail.com>
2022-09-08 12:09:08 +0900

    [add] test for join with real tpm

--- join_test.go
+++ join_test.go
@@ -4,15 +4,34 @@ import (
 	"testing"
 )
 
-func TestJoin(t *testing.T) {
+func TestJoinWithReal(t *testing.T) {
+	rng := InitRandom()
+
+	tpm, err := OpenRealTPM()
+	defer tpm.Close()
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	issuer := RandomIssuer(rng)
+	seed, err := issuer.genSeedForJoin(rng)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	member := NewMember(tpm)
+	_, err = member.genReqForJoin(seed, rng)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+}
+
+func TestJoinWithSW(t *testing.T) {
 	rng := InitRandom()
 
 	tpm := NewSWTPM(rng)
 	defer tpm.Close()
-
-	// if err != nil {
-	// 	t.Errorf("%v", err)
-	// }
 
 	issuer := RandomIssuer(rng)
 	seed, err := issuer.genSeedForJoin(rng)
