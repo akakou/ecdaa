5ed615382c8400f364c185c0c56fb6ac01f6c2f3
akakou <akakou571@gmail.com>
2022-09-22 03:46:56 +0900

    [update] use make credential

--- real_tpm_test.go
+++ real_tpm_test.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
+	"bytes"
 	"crypto/x509"
 	"fmt"
 	"testing"
 
-	legacy "github.com/google/go-tpm/legacy/tpm2"
 	"github.com/google/go-tpm/tpm2"
 )
 
@@ -52,78 +47,17 @@ func TestReadEKCert(t *testing.T) {
 }
 
 func TestActivateCredential(t *testing.T) {
-	// credential := []byte("hello")
-	// encCred := make([]byte, len(credential))
-	wrapedSecret := make([]byte, int(32))
-
 	tpm, err := OpenRealTPM()
-	defer tpm.Close()
-
 	if err != nil {
-		t.Errorf("%v", err)
+		t.Fatalf("could not connect to TPM simulator: %v", err)
 	}
 
-	iv := []byte("0123456789abcdef")
+	_, ekHandle, _, _ := tpm.CreateKey()
 
-	_, ekHandle, _, err := tpm.CreateKey()
-
-	if err != nil {
-		t.Errorf("%v", err)
-	}
-
-	cert, err := tpm.ReadEKCert()
-
-	if err != nil {
-		t.Errorf("%v", err)
-	}
-
-	pubkey := cert.PublicKey.(*rsa.PublicKey)
-
-	// 1. seed ←? Zp
-	seed := []byte("seed")
-
-	// 2. secret = F?(seed)
-	secret, err := legacy.KDFa(legacy.AlgSHA256, seed, "", nil, nil, 32*8)
-	if err != nil {
-		t.Errorf("%v", err)
-	}
-
-	// 3. encrypted_credential = enc(credential, secret)
-	// secretCipher, err := aes.NewCipher(secret)
-
-	// if err != nil {
-	// 	t.Errorf("%v", err)
-	// }
-
-	// secretCFBEncrypter := cipher.NewCFBEncrypter(secretCipher, iv)
-	// secretCFBEncrypter.XORKeyStream(encCred, credential)
-
-	// 5. hmac, symmetric_encryption_key = KDFa(seed, name)
-	symEncKey, err := legacy.KDFa(legacy.AlgSHA256, seed, "", nil, nil, 32*8)
-	if err != nil {
-		t.Errorf("%v", err)
-	}
-
-	symEncKeyCipher, err := aes.NewCipher(symEncKey)
-
-	if err != nil {
-		t.Errorf("%v", err)
-	}
-
-	symEncKeyCFBEncrypter := cipher.NewCFBEncrypter(symEncKeyCipher, iv)
-	symEncKeyCFBEncrypter.XORKeyStream(wrapedSecret, secret)
-
-	// 6. encrypted_seed = enc(seed, EK-C)
-	encSeed, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, pubkey, seed, []byte("IDENTITY"))
-	if err != nil {
-		t.Errorf("%v", err)
-	}
-
-	// read srkCreate key
 	srkCreate := tpm2.CreatePrimary{
 		PrimaryHandle: tpm2.TPMRHOwner,
 		InPublic: tpm2.TPM2BPublic{
-			PublicArea: tpm2.RSASRKTemplate,
+			PublicArea: tpm2.ECCSRKTemplate,
 		},
 	}
 
@@ -132,19 +66,27 @@ func TestActivateCredential(t *testing.T) {
 		t.Fatalf("could not generate SRK: %v", err)
 	}
 
-	// 5. activate credential
+	secret := tpm2.TPM2BDigest{Buffer: []byte("Secrets!!!")}
+
+	mc := tpm2.MakeCredential{
+		Handle:      ekHandle.Handle,
+		Credential:  secret,
+		ObjectNamae: srkCreateRsp.Name,
+	}
+
+	mcRsp, err := mc.Execute(tpm.tpm)
+	if err != nil {
+		t.Fatalf("could not make credential: %v", err)
+	}
+
 	ac := tpm2.ActivateCredential{
 		ActivateHandle: tpm2.NamedHandle{
 			Handle: srkCreateRsp.ObjectHandle,
 			Name:   srkCreateRsp.Name,
 		},
-		KeyHandle: *ekHandle,
-		CredentialBlob: tpm2.TPM2BIDObject{
-			Buffer: wrapedSecret,
-		},
-		Secret: tpm2.TPM2BEncryptedSecret{
-			Buffer: encSeed,
-		},
+		KeyHandle:      *ekHandle,
+		CredentialBlob: mcRsp.CredentialBlob,
+		Secret:         mcRsp.Secret,
 	}
 
 	acRsp, err := ac.Execute(tpm.tpm)
@@ -152,5 +94,7 @@ func TestActivateCredential(t *testing.T) {
 		t.Fatalf("could not activate credential: %v", err)
 	}
 
-	fmt.Printf("cert: %v\n", acRsp.CertInfo.Buffer)
+	if !bytes.Equal(acRsp.CertInfo.Buffer, secret.Buffer) {
+		t.Errorf("want %x got %x", secret.Buffer, acRsp.CertInfo.Buffer)
+	}
 }
