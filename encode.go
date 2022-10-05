c3f031642b53a0ce102f3aad011bacbddbf89039
akakou <akakou571@gmail.com>
2022-10-05 16:01:55 +0900

    [add] encode seed/join_request/cipher

--- encode.go
+++ encode.go
@@ -1,6 +1,11 @@
 package ecdaa
 
-import "miracl/core/FP256BN"
+import (
+	"crypto/x509"
+	"miracl/core/FP256BN"
+
+	"github.com/google/go-tpm/tpm2"
+)
 
 type MiddleEncodedISK struct {
 	X []byte
@@ -129,6 +134,124 @@ func (encoded *MiddleEncodedSignature) Decode() *Signature {
 	decoded.W = FP256BN.ECP_fromBytes(encoded.W)
 	decoded.E = FP256BN.ECP_fromBytes(encoded.E)
 	decoded.K = FP256BN.ECP_fromBytes(encoded.K)
+
+	return &decoded
+}
+
+type MiddleEncodedJoinSeeds struct {
+	Basename []byte
+	S2       []byte
+	Y2       []byte
+}
+
+func (seeds *JoinSeeds) Encode() *MiddleEncodedJoinSeeds {
+	var encoded MiddleEncodedJoinSeeds
+	encoded.Basename = seeds.Basename
+	encoded.S2 = seeds.S2
+	encoded.Y2 = bigToBytes(seeds.Y2)
+
+	return &encoded
+}
+
+func (encoded *MiddleEncodedJoinSeeds) Decode() *JoinSeeds {
+	var decoded JoinSeeds
+	decoded.Basename = encoded.Basename
+	decoded.S2 = encoded.S2
+	decoded.Y2 = FP256BN.FromBytes(encoded.Y2)
+
+	return &decoded
+}
+
+type MiddleEncodedJoinRequest struct {
+	Public  []byte
+	EKCert  []byte
+	C1      []byte
+	S1      []byte
+	N       []byte
+	Q       []byte
+	SrkName []byte
+}
+
+func (request *JoinRequest) Encode() *MiddleEncodedJoinRequest {
+	var encoded MiddleEncodedJoinRequest
+	public, err := tpm2.Marshal(request.Public)
+
+	if err != nil {
+		panic(err)
+	}
+
+	encoded.Public = public
+	encoded.EKCert = request.EKCert.Raw
+
+	encoded.C1 = bigToBytes(request.C1)
+	encoded.S1 = bigToBytes(request.S1)
+	encoded.N = bigToBytes(request.N)
+	encoded.Q = ecpToBytes(request.Q)
+
+	encoded.SrkName = request.SrkName
+
+	return &encoded
+}
+
+func (encoded *MiddleEncodedJoinRequest) Decode() (*JoinRequest, error) {
+	var decoded JoinRequest
+
+	var pub tpm2.TPM2BPublic
+	err := tpm2.Unmarshal(encoded.Public, &pub)
+
+	if err != nil {
+		return nil, err
+	}
+
+	decoded.Public = &pub
+	decoded.EKCert, err = x509.ParseCertificate(encoded.EKCert)
+
+	if err != nil {
+		return nil, err
+	}
+
+	decoded.C1 = FP256BN.FromBytes(encoded.C1)
+	decoded.S1 = FP256BN.FromBytes(encoded.S1)
+	decoded.N = FP256BN.FromBytes(encoded.N)
+	decoded.Q = FP256BN.ECP_fromBytes(encoded.Q)
+
+	decoded.SrkName = encoded.SrkName
+
+	return &decoded, nil
+}
+
+type MiddleEncodedCredCipher struct {
+	WrappedCredential []byte
+	IdObject          []byte
+	EncA              []byte
+	EncC              []byte
+	IV                []byte
+}
+
+func (cipher *CredCipher) Encode() *MiddleEncodedCredCipher {
+	var encoded MiddleEncodedCredCipher
+
+	encoded.WrappedCredential = cipher.WrappedCredential
+	encoded.IdObject = cipher.IdObject
+
+	encoded.EncA = cipher.EncA
+	encoded.EncC = cipher.EncC
+
+	encoded.IV = cipher.IV
+
+	return &encoded
+}
+
+func (encoded *MiddleEncodedCredCipher) Decode() *CredCipher {
+	var decoded CredCipher
+
+	decoded.WrappedCredential = encoded.WrappedCredential
+	decoded.IdObject = encoded.IdObject
+
+	decoded.EncA = encoded.EncA
+	decoded.EncC = encoded.EncC
+
+	decoded.IV = encoded.IV
 
 	return &decoded
 }
--- encode_test.go
+++ encode_test.go
@@ -1,9 +1,12 @@
 package ecdaa
 
 import (
+	"bytes"
 	"miracl/core"
 	"miracl/core/FP256BN"
 	"testing"
+
+	"github.com/google/go-tpm/tpm2"
 )
 
 func TestEncodeDecodeIPK(t *testing.T) {
@@ -139,5 +142,143 @@ func TestEncodeDecodeSignature(t *testing.T) {
 
 	if !signature.K.Equals(decoded.K) {
 		t.Fatalf("K is not equal")
+	}
+}
+
+func TestEncodeDecodeJoinSeeds(t *testing.T) {
+	var joinSeeds JoinSeeds
+
+	rnd := core.NewRAND()
+
+	joinSeeds.Basename = []byte("basename")
+	joinSeeds.S2 = randomBytes(rnd, 32)
+	joinSeeds.Y2 = FP256BN.Random(rnd)
+
+	encoded := joinSeeds.Encode()
+	decoded := encoded.Decode()
+
+	if !bytes.Equal(joinSeeds.Basename, decoded.Basename) {
+		t.Fatalf("basename is not equal")
+	}
+
+	if !bytes.Equal(joinSeeds.S2, decoded.S2) {
+		t.Fatalf("s2 is not equal")
+	}
+
+	if FP256BN.Comp(joinSeeds.Y2, decoded.Y2) != 0 {
+		t.Fatalf("y2 is not equal")
+	}
+}
+
+func TestEncodedDecodeJoinRequest(t *testing.T) {
+	var joinRequest JoinRequest
+
+	password := []byte("hoge")
+
+	tpm, err := OpenTPM(password, TPM_PATH)
+	defer tpm.Close()
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	cert, err := tpm.ReadEKCert()
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	rnd := core.NewRAND()
+
+	joinRequest.EKCert = cert
+
+	param := publicParams()
+
+	joinRequest.Public = &tpm2.TPM2BPublic{
+		PublicArea: param.key,
+	}
+
+	joinRequest.C1 = FP256BN.Random(rnd)
+	joinRequest.S1 = FP256BN.Random(rnd)
+	joinRequest.N = FP256BN.Random(rnd)
+	joinRequest.Q = randomECP(rnd)
+	joinRequest.SrkName = randomBytes(rnd, 32)
+
+	encoded := joinRequest.Encode()
+	decoded, err := encoded.Decode()
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	reEncoded, err := tpm2.Marshal(joinRequest.Public)
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if !bytes.Equal(joinRequest.EKCert.RawIssuer, decoded.EKCert.RawIssuer) {
+		t.Fatalf("EKCert is not equal")
+	}
+
+	if !bytes.Equal(
+		encoded.Public,
+		reEncoded) {
+		t.Fatalf("Public is not equal")
+	}
+
+	if FP256BN.Comp(joinRequest.C1, decoded.C1) != 0 {
+		t.Fatalf("C1 is not equal")
+	}
+
+	if FP256BN.Comp(joinRequest.S1, decoded.S1) != 0 {
+		t.Fatalf("S1 is not equal")
+	}
+
+	if FP256BN.Comp(joinRequest.N, decoded.N) != 0 {
+		t.Fatalf("N is not equal")
+	}
+
+	if !joinRequest.Q.Equals(decoded.Q) {
+		t.Fatalf("Q is not equal")
+	}
+
+	if !bytes.Equal(joinRequest.SrkName, decoded.SrkName) {
+		t.Fatalf("SrkName is not equal")
+	}
+}
+
+func TestEncodedDecodeCredCipher(t *testing.T) {
+	var credCipher CredCipher
+
+	rnd := core.NewRAND()
+
+	credCipher.WrappedCredential = randomBytes(rnd, 32)
+	credCipher.IdObject = randomBytes(rnd, 32)
+	credCipher.EncA = randomBytes(rnd, 32)
+	credCipher.EncC = randomBytes(rnd, 32)
+	credCipher.IV = randomBytes(rnd, 32)
+
+	encoded := credCipher.Encode()
+	decoded := encoded.Decode()
+
+	if !bytes.Equal(credCipher.WrappedCredential, decoded.WrappedCredential) {
+		t.Fatalf("WrappedCredential is not equal")
+	}
+
+	if !bytes.Equal(credCipher.IdObject, decoded.IdObject) {
+		t.Fatalf("IdObject is not equal")
+	}
+
+	if !bytes.Equal(credCipher.EncA, decoded.EncA) {
+		t.Fatalf("EncA is not equal")
+	}
+
+	if !bytes.Equal(credCipher.EncC, decoded.EncC) {
+		t.Fatalf("EncC is not equal")
+	}
+
+	if !bytes.Equal(credCipher.IV, decoded.IV) {
+		t.Fatalf("IV is not equal")
 	}
 }
